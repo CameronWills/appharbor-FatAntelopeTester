96b1956da949a9130d7d33960fd0d538843ff5be
Cameron Wills <cwills@nrl.com.au>
2016-08-11 12:57:47 +1000

    Updated version of FatAntelope.Core. Added version string to UI.

--- FatAntelope.WebTester/Controllers/HomeController.cs
+++ FatAntelope.WebTester/Controllers/HomeController.cs
@@ -1,14 +1,30 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Reflection;
+using System.Web.Mvc;
 
 namespace FatAntelope.WebTester.Controllers
 {
+
 	[RequireHttps]
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
+        private static Lazy<string> version = new Lazy<string>(() =>
+        {
+            try
+            {
+                return "v" + typeof(XTree).Assembly.GetName().Version.ToString(3);
+            }
+            catch (Exception)
+            { }
+
+            return string.Empty;
+        });
+
+        public ActionResult Index()
 		{
             ViewBag.DefaultSourceConfig = Resources.DefaultSourceConfig;
             ViewBag.DefaultTargetConfig = Resources.DefaultTargetConfig;
+            ViewBag.Version = version.Value;
 			return View();
 		}
 	}
